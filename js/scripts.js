cb7e56f1e354effdc89c7aca7835fb3e6cbb087e
ambrosia-fish <josefameur@gmail.com>
2024-05-14 15:16:16 -0400

    Add pokemonList
    Removed old code
    Added Bulbasaur, Charmander and Squirtle to pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+let pokemonList = [
+    {
+        pokemonName: 'Bulbasaur', 
+        height: '2\'04\"', 
+        type: [
+            'Grass',
+            'Poison',
+        ]  
+    },
+    {
+        pokemonName: 'Charmander',
+        height: '2\'0\"',
+        type: 'Fire',
+    },
+    {
+        pokemonName: 'Squirtle',
+        height: '1\'0\"',
+        type: 'Water',
+
+    }
+]
