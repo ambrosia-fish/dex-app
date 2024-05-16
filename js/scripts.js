ca0b9d63259ee0d6cb313066c3a8aff283690cb1
ambrosia-fish <josefameur@gmail.com>
2024-05-15 20:00:37 -0400

    Created loop that displays pokemonName and height
    If height is taller than .7 meters, add "Whoa that is big!" message/

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,13 @@ let pokemonList = [
 
     }
 ]
+
+// loop that writes following string to DOM "_____ is _____ tall."
+for (let i = 0; i < 3; i++) {
+    document.write(pokemonList[i].pokemonName + " is " + pokemonList[i].height + " tall.")
+    // if the pokemon is taller than 0.7 meters, highlight it with message.
+    if (pokemonList[i].height > .7)
+        document.write(" Whoa! That is big!")
+    // insert line break
+    document.write("<br>")
+}
