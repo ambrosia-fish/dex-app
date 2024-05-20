a132ff53563cdf01bf3c303d590ec3b7b5fc8de8
ambrosia-fish <josefameur@gmail.com>
2024-05-20 17:08:46 -0400

    Added console.log and document.write for testing

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,10 @@ let pokemonRepository = (function () {
     };
 })();
 
+console.log(pokemonRepository.getAll())
+
+document.write(pokemonRepository.getAll())
+
 /* forEach loop from 2.1.4
 pokemonList.forEach(function(pokemon) {
     document.write(pokemon.name + " is " + pokemon.height + " tall.");
