3ebe2c394436fa5d7de2c62d6ba5b997d724f45d
ambrosia-fish <josefameur@gmail.com>
2024-05-20 16:55:52 -0400

    Added return functions to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,21 @@ let pokemonRepository = (function () {
             height: .5,
             type: 'Water',
 
-    }
-]
+        }
+    ]
 
 // loop that writes following string to DOM "_____ is _____ tall."
 /* for (let i = 0; i < 3; i++) {
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        }
+    };
+})();
+
     document.write("<p>")
     document.write(pokemonList[i].pokemonName + " is " + pokemonList[i].height + " tall.")
     // if the pokemon is taller than 0.7 meters, highlight it with message.
