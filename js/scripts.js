a279d384d125b1f36391f432e5623ec29c66e628
ambrosia-fish <josefameur@gmail.com>
2024-05-29 12:21:32 -0400

    Properly declared all variables

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ let pokemonRepository = (function () {
 
     /* Loadlist function fires immediately via the function outside the IIFE. Pulls data from pokemon API and creates a pokemon object with name
     and details url. Fires add function. */
-    function loadList(item) {
+    function loadList() {
         return fetch(apiURL).then(function (response) {
             return response.json();
         }).then(function (json) {
@@ -48,24 +48,32 @@ let pokemonRepository = (function () {
     /* loadDetails takes a pokemon object, fetches/parses JSON pokemon metadata from the profile URL, 
        and creates four new objects for bio details. Includes a catch function to log any errors. */
     function loadDetails(pokemon) {
-        let url = pokemon.detailsUrl;
-        return fetch(url).then(function (response) {
-            return response.json();
-        }).then(function (detail) {
-            bioImgUrl = detail.sprites.front_default;
-            bioHeight = detail.height;
-            bioWeight = detail.weight;
-        }).catch(function (e) {
-            console.error(e);
-        }).then(function (){
+    let bioImgUrl, bioHeight, bioWeight;
+
+    let url = pokemon.detailsUrl;
+    return fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (detail) {
+        bioImgUrl = detail.sprites.front_default;
+        bioHeight = detail.height;
+        bioWeight = detail.weight;
+        return { bioImgUrl, bioHeight, bioWeight };
+    }).catch(function (e) {
+        console.error(e);
+    }).then(function () {
+        if (bioImgUrl && bioHeight && bioWeight) {
+            let displayImage = bioImgUrl;
+            let displayHeight = bioHeight;
+            let displayWeight = bioWeight;
             let modalTitle = document.querySelector('.modal-title');
             modalTitle.innerText = pokemon.name;
             let modalBody = document.querySelector('.modal-body');
             // set the modal's body to include an image of the pokemon (with an alt for screenreaders) along with height and weight
-            modalBody.innerHTML = '<img alt="' + pokemon.name + '" src="' + bioImgUrl + '"</img><br>' + 'Height: ' + bioHeight + '<br>' + 'Weight: ' + bioWeight;
-        });
+            modalBody.innerHTML = '<img alt="' + pokemon.name + '" src="' + displayImage + '"></img><br>' + 'Height: ' + displayHeight + '<br>' + 'Weight: ' + displayWeight;
+        }
+    });
+};
 
-    };
 
     /* returns key/value pairs for external access to repository functions s */
     return {
